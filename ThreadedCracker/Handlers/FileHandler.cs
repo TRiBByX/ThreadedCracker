f015b477347b3e0d788b9b4d356fa3cafce9abfc
TRiBByX <christoffer.ssj@gmail.com>
2017-03-13 17:07:41 +0100

    Small alterations

--- ThreadedCracker/Handlers/FileHandler.cs
+++ ThreadedCracker/Handlers/FileHandler.cs
@@ -11,7 +11,7 @@ namespace ThreadedCracker.Handlers
         public static List<UserData> Passwords = new List<UserData>();
 
 
-        private const int MaxChunckSize = 60000;
+        private const int MaxChunckSize = 6000;
 
         public static void LoadDictionary()
         {
